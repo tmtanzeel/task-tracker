5ff7e4b7ed125eba4952cbf2fa6ea1ec6a80f35b
Tanzeel Mirza <tmtanzeel9@gmail.com>
2020-02-20 14:03:47 +0530

    added two buttons

--- MainFrame.java
+++ MainFrame.java
@@ -1,6 +1,9 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -9,12 +12,25 @@ import javafx.stage.Stage;
 public class MainFrame extends Application {
 
     Scene scene;
+    BorderPane borderPaneLayout = new BorderPane();
+    Button addNewBranch, showBranchStatus;
+    HBox horizontalLayout1, horizontalLayout2;
 
     @Override
-    public void start(Stage primarystage) {
+    public void start(Stage primarystage) {        
 
-        GridPane layout = new GridPane();
-        scene = new Scene(layout, 350, 200);
+        addNewBranch=new Button("Add");
+        showBranchStatus = new Button("Status");
+
+        horizontalLayout2 = new HBox(10);
+        horizontalLayout2.setAlignment(Pos.CENTER);
+        horizontalLayout2.getChildren().addAll(addNewBranch, showBranchStatus);
+
+        borderPaneLayout = new BorderPane();
+        borderPaneLayout.setBottom(horizontalLayout2);
+
+        scene = new Scene(borderPaneLayout, 350, 200);
+
         primarystage.setScene(scene);
         primarystage.setTitle("TASK MANAGER");
         primarystage.setMinWidth(1000);
